8671ebf5d72a480c120fe204af220322cf897154
Moons <me@moons.bio>
2025-04-29 18:23:45 +0200

    refactor: remove unused components and update configurations
    Deleted unused components (`chart-area-interactive.tsx`, `data-table.tsx`, `chart.tsx`) and associated JSON data file (`data.json`). Updated `next.config.ts`, `postcss.config.mjs`, and `site-header.tsx` for consistency. Added new `randomizer/page.tsx` and modified `page.tsx` to redirect to `/randomizer`. This cleanup improves maintainability and reduces unnecessary code.

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -17,7 +17,7 @@ export function SiteHeader() {
         <div className="ml-auto flex items-center gap-2">
           <Button variant="ghost" asChild size="sm" className="hidden sm:flex">
             <a
-              href="https://github.com/"
+              href="https://github.com/MoonieGZ/Echovia"
               rel="noopener noreferrer"
               target="_blank"
               className="dark:text-foreground"
